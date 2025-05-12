b81083df0620a4cd45eefb97d6fa1f3844bf179e
kazyosha <chitampham07@gmail.com>
2025-05-12 10:14:11 +0700

    5/12 Lan 3

---
+++ Javascript/java.js
@@ -0,0 +1,10 @@
+let inPutWidth;
+let inPutHeight;
+
+inPutWidth = prompt("Vui long ghi can nang cua ban:");
+inPutHeight = prompt("Vui long ghi chieu cao cua ban:");
+
+let width = parseInt(inPutWidth);
+let height = parseInt(inPutHeight);
+let area = width * height;
+document.write(area);
