c1cb29862cf6505f6f57a7cc6e94a91bbae883e7
kazyosha <chitampham07@gmail.com>
2025-05-13 15:59:35 +0700

    5/13 Lan 2

--- Javascript/java.js
+++ Javascript/java.js
@@ -1,10 +1,67 @@
-let inPutWidth;
-let inPutHeight;
 
-inPutWidth = prompt("Vui long ghi can nang cua ban:");
-inPutHeight = prompt("Vui long ghi chieu cao cua ban:");
+function heightAndWidth() {
+    let width = +document.getElementById('width').value;
+    let height = +document.getElementById('heigh').value;
+    let area = width * height;
+    document.getElementById('ketQua').innerHTML ='Ket qua la: ' + area;
+}
 
-let width = parseInt(inPutWidth);
-let height = parseInt(inPutHeight);
-let area = width * height;
-document.write( 'Ket qua:' + " " + area );
+function year() {
+    let input = document.getElementById("nam").value;
+    let y = parseInt(input);
+
+
+    if (input.trim() === "") {
+        document.getElementById('ketQua').innerHTML = " Vui lòng nhập năm cần kiểm tra.";
+    }
+    else if (y > 0){
+        if ((y % 4 === 0 && y % 100 !== 0) || (y % 400 === 0)) {
+            document.getElementById("ketQua").innerHTML = 'Năm ' + y + ': Là Năm Nhuận';
+        } else {
+            document.getElementById("ketQua").innerHTML = 'Năm ' + y + ': Là Năm Không Nhuận';
+        }
+    } else {
+        document.getElementById('ketQua').innerHTML = "Năm phải là số dương"
+    }
+}
+function year1() {
+    let input1 = document.getElementById("nam").value;
+    let y = parseInt(input1);
+
+    if (input1.trim() === "") {
+        document.getElementById('ketQua').innerHTML = " Vui lòng nhập năm cần kiểm tra.";
+    }
+    else if (y > 0){
+        if (y % 4 ===0 && y % 100 !== 0 ) {
+            document.getElementById("ketQua").innerHTML = 'Năm ' + y + ': Là Năm Nhuận';
+        } else {
+            document.getElementById("ketQua").innerHTML = 'Năm ' + y + ': Là Năm Không Nhuận';
+        }
+    } else {
+        document.getElementById('ketQua').innerHTML = "Năm phải là số dương"
+    }
+
+}
+function login() {
+  let userName = document.getElementById("loginUser").value;
+
+  if (userName === 'Tam'|| userName === 'Cuong') {
+      let password = document.getElementById("password").value;
+      if (password === '123') {
+          document.getElementById("ketQua").innerHTML= "Chao Mừng:" + userName;
+      }
+      else if (password === "") {
+          document.getElementById('ketQua').innerHTML = 'Vui lòng điền Password'
+      }
+      else {
+          document.getElementById('ketQua').innerHTML = "Sai mật khẩu";
+      }
+  }
+  else if (userName === "") {
+      document.getElementById("ketQua").innerHTML = "Vui lòng điền tên tài khoản của bạn";
+  }
+  else {
+      document.getElementById('ketQua').innerHTML = "Sai tên tài khoản, vui lòng thử lại"
+  }
+
+}
