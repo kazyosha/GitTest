3e0f7d04cf958b5f172cde7fc2b9b0769971b8d4
kazyosha <chitampham07@gmail.com>
2025-05-14 09:43:40 +0700

    5/14 Lan 1

--- Javascript/java.js
+++ Javascript/java.js
@@ -97,3 +97,53 @@ function numSimple(){
             document.getElementById('ketQua').innerHTML = number + " " + "là số bạn chọn"
     }
 }
+function  BMI(){
+    let width = document.getElementById("width").value;
+    let heigh = document.getElementById("heigh").value;
+    // let bmi = width / (heigh ^ 2) ;
+    let bmi = width / ((heigh / 100) * (heigh / 100));
+
+    if (bmi < 18) {
+        document.getElementById('ketQua1').innerHTML = bmi + " " +':Can nang cua ban Underweight'
+    }
+    else if (bmi < 25) {
+        document.getElementById('ketQua1').innerHTML = bmi + " " +':Can nang cua ban Normal'
+    }
+    else if (bmi < 30) {
+        document.getElementById('ketQua1').innerHTML = bmi + " " +':Can nang cua ban Overweight'
+    }
+    else {
+        document.getElementById('ketQua1').innerHTML = bmi + " " +':Can nang cua ban Obese'
+    }
+}
+function Month(){
+    let dayOfMonth = document.getElementById("month").value;
+    switch (dayOfMonth) {
+        case '1':
+        case '3':
+        case '5':
+        case '7':
+        case '8':
+        case '10':
+        case '12':
+            document.getElementById("ketQua").innerHTML ="Tháng " + dayOfMonth +" "+"có 31 ngày";
+            break;
+        case '4':
+        case '6':
+        case '9':
+        case '11':
+            document.getElementById("ketQua").innerHTML ="Tháng " + dayOfMonth +" "+"có 30 ngày";
+            break;
+        default:
+            document.getElementById("ketQua").innerHTML ="Tháng " + dayOfMonth +" "+"có 28 ngày";
+    }
+}
+function yesClick(){
+    alert("Yes i do");
+}
+function noClick(){
+    let a = Math.round(Math.random() * window.innerWidth);
+    let b = Math.round(Math.random() * window.innerHeight);
+    document.getElementById("btnNo").style.left =  a + 'px';
+    document.getElementById("btnNo").style.top =  b + 'px';
+}
